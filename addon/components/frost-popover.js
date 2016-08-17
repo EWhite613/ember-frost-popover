8be70e651ae3ecb1ba91938aec11cd70ff752e62
Eric White <ericwhite613@gmail.com>
2016-08-17 12:02:45 -0400

    Cleanup

--- addon/components/frost-popover.js
+++ addon/components/frost-popover.js
@@ -28,7 +28,6 @@ export default Ember.Component.extend({
         result = 'middle left'
         break
     }
-    console.log('targetAttachment', result)
     return result
   }),
   attachment: Ember.computed('position', function () {
@@ -48,22 +47,11 @@ export default Ember.Component.extend({
         result = 'middle right'
         break
     }
-    console.log('attachment', result)
     return result
   }),
   didInsertElement () {
-    console.log('init')
     Ember.run.next(() => {
       const context = this
-      // if (this.get('closest')) {
-      //   this.$().closest(this.get('target')).on(this.get('event'), function () {
-      //     context.send('togglePopover')
-      //   })
-      // } else {
-      //   this.get('parentView').$(this.get('target')).on(this.get('event'), function () {
-      //     context.send('togglePopover')
-      //   })
-      // }
       $(this.get('target')).on(this.get('event'), function () {
         context.send('togglePopover')
       })
@@ -76,39 +64,6 @@ export default Ember.Component.extend({
     },
     togglePopover () {
       this.toggleProperty('visible')
-      // let position = this.get('position')
-      // let targetRect
-      // targetRect = this.get('closest') ? this.$().closest(this.get('target'))[this.get('index')].getBoundingClientRect() : this.get('parentView').$(this.get('target'))[this.get('index')].getBoundingClientRect()
-      // let popoverElement = this.get('element')
-      // let popoverRect = popoverElement.getBoundingClientRect()
-      // let top
-      // let left
-      // switch (position) {
-      //   case 'bottom':
-      //     top = targetRect.bottom + 5
-      //     left = targetRect.left + targetRect.width / 2 - popoverRect.width / 2
-      //     popoverElement.style.top = top + 'px'
-      //     popoverElement.style.left = left + 'px'
-      //     break
-      //   case 'top':
-      //     top = targetRect.top - targetRect.height - 5
-      //     left = targetRect.left + targetRect.width / 2 - popoverRect.width / 2
-      //     popoverElement.style.top = top + 'px'
-      //     popoverElement.style.left = left + 'px'
-      //     break
-      //   case 'left':
-      //     top = targetRect.top + targetRect.height / 2 - popoverRect.height / 2
-      //     left = targetRect.left - targetRect.width + 34.4
-      //     popoverElement.style.top = top + 'px'
-      //     popoverElement.style.left = left + 'px'
-      //     break
-      //   case 'right':
-      //     top = targetRect.top + targetRect.height / 2 - popoverRect.height / 2
-      //     left = targetRect.right + 5
-      //     popoverElement.style.top = top + 'px'
-      //     popoverElement.style.left = left + 'px'
-      //     break
-      // }
     }
   }
 })
